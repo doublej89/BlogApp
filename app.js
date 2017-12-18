989ad40aa40112ad1d6bdc75ce93bea454f0dd67
copperware <jawad.jahangir.666@gmail.com>
2017-12-19 04:27:36 +0600

    added post delete functionality and tied up loose ends

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
 	express = require("express"),
 	methodOverride = require("method-override"),
+	expressSanitizer = require("express-sanitizer"),
 	app = express();
 
 mongoose.connect("mongodb://localhost/blog_app", {
@@ -10,6 +11,7 @@ mongoose.connect("mongodb://localhost/blog_app", {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
@@ -49,6 +51,7 @@ app.get("/blogs/:id", function(req, res) {
 });
 
 app.post("/blogs", function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog) {
 		if (err) {
 			res.render("new");
@@ -69,11 +72,22 @@ app.get("/blogs/:id/edit", function(req, res) {
 });
 
 app.put("/blogs/:id", function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
 		if (err) {
 			res.redirect("/blogs");
 		} else {
 			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
+app.delete("/blogs/:id", function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
 		}
 	});
 });
